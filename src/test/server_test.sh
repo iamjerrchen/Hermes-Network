5ec7db929646646384b7add4a373fd1cf99c7c5c
jerr-chen <jerrychen31@gmail.com>
2018-04-13 00:15:18 -0500

    Changed executable destination

--- src/test/server_test.sh
+++ src/test/server_test.sh
@@ -1,2 +1,2 @@
-g++ ../temp/client.cpp -o client
-g++ server_test.cpp ../main/socket.cpp -o server
+g++ ../temp/client.cpp -o ../../bin/client
+g++ server_test.cpp ../main/socket.cpp -o ../../bin/server
