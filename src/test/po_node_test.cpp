6f063c30701f8f1243e81ade39731c6be9fa6b85
jerr-chen <jerrychen31@gmail.com>
2018-04-21 17:26:48 -0500

    Blank structured test file

---
+++ src/test/po_node_test.cpp
@@ -0,0 +1,18 @@
+/* po_node_test.cpp
+ *
+ *
+ *
+ */
+
+#include "../main/po_node.h"
+
+
+int main()
+{
+
+	// test add_neighbor to make sure neighbor_node_ips are filled properly.
+
+
+
+	return 0;
+}
