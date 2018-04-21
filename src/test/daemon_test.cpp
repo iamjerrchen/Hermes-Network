cbda4204837bf900a3454250d17e613af7d5ae8f
jerr-chen <jerrychen31@gmail.com>
2018-04-21 15:28:39 -0500

    Tested daemonize with server socket code.

--- src/test/daemon_test.cpp
+++ src/test/daemon_test.cpp
@@ -5,23 +5,50 @@
  */
 
 #include "../main/daemonize.h"
+// special daemonized server includes
+#include "../main/socket.h"
+
+#include <string.h>
+// end special daemonized server includes
 
 #include <syslog.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+void daemon_server()
+{
+	int new_socket, valread;
+
+	char buffer[1024] = {0};
+	char *hello = "Hello from server";
+
+	Socket s;
+	s.setup_server_socket(31337);
+
+	new_socket = s.accept_conn();
+	valread = read(new_socket,buffer, 1024);
+	syslog(LOG_INFO, "[daemon_test] received message: %s", buffer);
+	send(new_socket, hello, strlen(hello), 0);
+	syslog(LOG_INFO, "[daemon_test] Hello message sent.");
+
+	return;
+}
 
 int main()
 {
 	setup_daemon();
 	while(1)
 	{
+		syslog(LOG_NOTICE, "[daemon_test] First daemon started.");
+
 		//TODO: insert daemon code here
-		syslog(LOG_NOTICE, "First daemon started.");
+		//daemon_server();
+
 		sleep(20);
 		break;
 	}
 
-	syslog(LOG_NOTICE, "First daemon terminated.");
+	syslog(LOG_NOTICE, "[daemon_test] First daemon terminated.");
 	teardown_daemon();
 
 	return EXIT_SUCCESS;
