a01431e0dbd45c2dd8567616cc431e4f6005a610
Tim <tdh911@gmail.com>
2018-05-01 19:01:58 -0500

    Add main

---
+++ server/src/main/main.cpp
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+	if (argc > 4)
+		exit(EXIT_FAILURE);
+	// Create client connection
+	
+	// Create node connections
+	
+	// create server socket
+	while (true) {
+		// if number of connections < 3, have server accept
+		// ensure threads is always <= 3
+
+	}
+}
