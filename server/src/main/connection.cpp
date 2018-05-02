2fb95351d768799e4ca3a213b0b8d3b07d3cba79
Rohit Thotakura <rthotakura97@gmail.com>
2018-05-01 21:00:12 -0500

    template for connection

--- server/src/main/connection.cpp
+++ server/src/main/connection.cpp
@@ -1,2 +1,39 @@
 
 #include "connection.h"
+
+Connection::Connection(int fd, std::string ip, global_data * data)
+{    
+    // set class variables
+    this->fd = fd;
+    this->ip = ip;
+    this->data = data;
+}
+
+Connection::Connection(int fd, global_data * data)
+{
+    // create an fd from the socket object
+    this->fd = fd;
+    this->ip = "";
+    this->data = data;
+}
+
+// send a message to intiate neighbor
+bool Connection::greet_neighbor()
+{
+
+}
+
+bool Connection::receive_message()
+{
+
+}
+
+bool Connection::send_message()
+{
+
+}
+
+bool Connection::handle_message()
+{
+
+}
--- server/src/main/connection.h
+++ server/src/main/connection.h
@@ -1,15 +1,31 @@
 #ifndef P2P_MAIN_CONNECTION_H_
 #define P2P_MAIN_CONNECTION_H_
 
+#include <string.h>
 
+#include "socket.h"
+#include "global_data.h"
+class Connection {
 
+private:
 
+    int fd; // file descriptor
+    std::string ip; // ip address of other side of connection
+    global_data * data; // pointer to global map of messages
 
+public:
 
+    // constructor and destructor
+    Connection(int fd, std::string ip, global_data * data);
+    ~Connection();
+    
 
+    // methods to interface with messages
+    bool greet_neighbor();
+    bool receive_message();
+    bool send_message();
+    bool handle_message();
 
-
-
-
+};
 
 #endif
