a9e58303221224e6ca2ebea9e8a0ef4a8d232d63
Rohit Thotakura <rthotakura97@gmail.com>
2018-05-01 21:08:07 -0500

    added to template

--- server/src/main/connection.cpp
+++ server/src/main/connection.cpp
@@ -15,16 +15,19 @@ bool Connection::greet_neighbor()
 
 }
 
+// receive and handle messages
 bool Connection::receive_message()
 {
 
 }
 
+// send a message to neighbor
 bool Connection::send_message()
 {
 
 }
 
+// place message into global data structure
 bool Connection::handle_message()
 {
 
